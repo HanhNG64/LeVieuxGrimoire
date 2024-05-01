b43ddb454db557311ed8d6f734123e6b856d5e34
Hanh NG <moitmp64@gmail.com>
2024-05-01 19:46:21 +0200

    mettre en place Helmet

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,20 @@ const bookRoute = require('./routes/book.js');
 const userRoute = require('./routes/user.js');
 const path = require('path');
 const fs = require('fs');
+const helmet = require('helmet');
 
 // Allow the Express application to process JSON data from requests
 const app = express();
 app.use(express.json());
 
-// Create the images directory to store images
-const imagesDir = path.join(__dirname, 'images');
-if (!fs.existsSync(imagesDir)) {
-  fs.mkdirSync(imagesDir);
-}
+// Secure HTTP headers against XSS attacks
+app.use(helmet());
+// Enable CORS configuration with Helmet.js and allow cross-origin queries
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: 'cross-origin',
+  }),
+);
 
 // Configure CORS headers to allow access to the API from any origin
 app.use((req, res, next) => {
@@ -21,6 +25,12 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
+
+// Create the images directory to store images
+const imagesDir = path.join(__dirname, 'images');
+if (!fs.existsSync(imagesDir)) {
+  fs.mkdirSync(imagesDir);
+}
 
 app.use('/api/auth', userRoute);
 app.use('/api/books', bookRoute);
